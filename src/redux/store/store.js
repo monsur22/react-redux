b198442cf9b1405b8b45def4fc491e68316eab46
monsur22 <monsurahmedshafiq@gmail.com>
2023-09-07 21:31:12 +0600

    redux todo with style

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -7,12 +7,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../reducer/counterReducer";
 import todoReducer from "../reducer/todoReducer";
+import productReducer from "../reducer/productReducer";
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         todo: todoReducer,
-        // Add other reducers here if needed
+        product: productReducer,
+
     },
 });
 
